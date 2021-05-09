8da4e302e6528442dbce8538d0ebd18abc11e65c
Anton Shumakov <47758828+anton-shumakov@users.noreply.github.com>
2021-05-09 10:43:06 +0300

    Update UI.py

--- src/UI.py
+++ src/UI.py
@@ -23,6 +23,17 @@ class Form:
 			result.append(line)
 		return result
 
+	def set_string(self, string, y, x):
+		iterator = 0
+		y = 1 if y == 0 else y
+		for i in range(y, self.height - 1):
+			for j in range(1, self.width - 1):
+				if iterator >= len(string):
+					return
+				self.image[i][j] = string[iterator]
+				iterator += 1
+				
+
 
 
 class UI:
@@ -33,18 +44,22 @@ class UI:
 	def render_background_with_form(self, form):
 		x = self.background.x + (self.background.width - form.width) // 2
 		y = self.background.y + (self.background.height - form.height) // 2
-		for i in range(4):
+		for i in range(2):
 			self.background.render_window(x=x, y=y, width=form.width, height=form.height, image=form.image)
 			time.sleep(1)
 			self.background.push_new_line_on_window()
 		
 	def run(self):
-		form = Form(10, 10)
+		form = Form(20, 20)
 		thread = threading.Thread(target=self.render_background_with_form,args=([form]))
 		thread.start()
-		# a = input()
-		# form.set('Enter size:', *(0, 0))
 
+		form.set_string('Enter size:' * 3, *(1, 1))
+		# size = input()
+		data = self.simulator.get_data(3)
+		initial = self.simulator.get_initial(data)
+		form.set_string(initial, *(2, 1))
+		# response = input()
 
 		# for i in range(len(data)):
 		# 	for j in range(len(data[0])):
